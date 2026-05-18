5b2103bc0eae288c0335a2141191d1a20a135166
RAE PUTRI KAVI <raeputrikavi1313@gmail.com>
2026-05-18 12:00:21 +0700

    MAMBO LUAS SEGITIGA
    bersama mambo mari mengitung luas segitiga

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,10 @@
 import streamlit as st
 
-st.title("🎈MAMBOOOOOOOOOO")
+st.title("🎈BERSAMA MAMBO MENGHITUNG LUAS SEGITIGA")
 st.write(
     "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
 )
+a=float(input("masukan al='))
+t=float(input("masukan tinggi="))
+1=0.5*a*t
+  print ("luas segitiga=",1)
