bc5555a770db44006dcd3f3434fbce41d897b71a
RAE PUTRI KAVI <raeputrikavi1313@gmail.com>
2026-05-18 12:05:11 +0700

    BERSAMA MAMBO MENGHITUNG SEGITIGA

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,7 @@
 import streamlit as st
 
 st.title("🎈BERSAMA MAMBO MENGHITUNG LUAS SEGITIGA")
-a=float(input("masukan alas='))
+a=float(input("masukan alas="))
 t=float(input("masukan tinggi="))
 1=0.5*a*t
 print ("luas segitiga=",1)
